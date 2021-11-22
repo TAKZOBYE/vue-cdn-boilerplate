83921b48f27cb3382d4db8a507a0268978201d49
TAKZOBYE <takerchanel@gmail.com>
2021-11-23 02:58:58 +0700

    Data styling

--- about.js
+++ about.js
@@ -1,9 +1,19 @@
 const About = Vue.component('About', {
     template: /* html */ `
-        <div @click="print"> 
+        <div :style="style.container"> 
             awfawf
         </div>
     `,
+    data() {
+        return {
+            style: {
+                container: {
+                    color: white,
+                    backgroundColor: 'red'
+                }
+            }
+        }
+    },
     methods: {
         print() {
             console.log('print')
